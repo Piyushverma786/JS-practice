0f79d2ee985390369a378fc8caec61edd4acd3dc
Piyushverma786 <142714120+Piyushverma786@users.noreply.github.com>
2025-07-21 00:11:41 +0530

    hello

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const JWT_SECRET = "hellopiyush";
 app.use(express.json());
 const users = [];
 
+
 app.post("/signup", function(req, res) {
     const username = req.body.username;
     const password = req.body.password;
